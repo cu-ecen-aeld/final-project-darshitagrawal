bfff94725a46910459e615315f5898d0daadbf5c
Darshit Agrawal <daag8293@colorado.edu>
2022-04-10 18:44:21 -0600

    Sprint-2 Final

---
+++ base_external/package/humidity/humidity.mk
@@ -0,0 +1,26 @@
+
+##############################################################
+#
+# HUMIDITY_SENSOR
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+HUMIDITY_VERSION = e8908a46fc2fc165c71a90b207119fdc2617a1d7
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+HUMIDITY_SITE = git@github.com:cu-ecen-aeld/darshitagrawal_hwsingh27_shared.git
+HUMIDITY_SITE_METHOD = git
+HUMIDITY_GIT_SUBMODULES = YES
+
+define HUMIDITY_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/humidity all
+endef
+
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+define HUMIDITY_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/humidity/humiditytest $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
---
+++ base_external/package/mlx90614/mlx90614.mk
@@ -0,0 +1,26 @@
+
+##############################################################
+#
+# MLX90614
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+MLX90614_VERSION = 16c5c8d8359cf12d7f8531124c815597eaffa331
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+MLX90614_SITE = git@github.com:cu-ecen-aeld/darshitagrawal_hwsingh27_shared.git
+MLX90614_SITE_METHOD = git
+MLX90614_GIT_SUBMODULES = YES
+
+define MLX90614_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/mlx90614 all
+endef
+
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+define MLX90614_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/mlx90614/mlx90614test $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
